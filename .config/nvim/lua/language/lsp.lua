80c8dd0c5f247329dbdd3bca6dd47b3cd6e25427
root <root@cc>
2023-05-01 22:31:57 -0400

    enable mason lsp

--- .config/nvim/lua/language/lsp.lua
+++ .config/nvim/lua/language/lsp.lua
@@ -1,12 +1,18 @@
-local lang = require('language.lang')
+local lsp = require("lsp-zero")
+local lang = require("language.lang")
+lsp.preset("recommended")
+lsp.setup()
 
-require('nvim-treesitter.configs').setup({
-	ensure_installed = lang.alllang,
-	-- ignore_install = lang.experimentallang,
-	sync_install = false,
-	auto_install = true,
-		highlight = {
-		enable = true
-	},
-	additional_vim_regex_highlighting = false,
-})
+require("nvim-treesitter.configs").setup(
+    {
+        ensure_installed = lang.languages,
+        ignore_install = lang.experimentallang,
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+            enable = true
+        },
+        additional_vim_regex_highlighting = false
+    }
+)
+
---
+++ .config/nvim/lua/language/snippet.lua
@@ -0,0 +1,37 @@
+local cmp = require("cmp")
+local cmap = cmp.mapping
+local window = cmp.config.window
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
+cmp.setup(
+    {
+        window = {
+            completion = window.bordered()
+        },
+        mapping = cmap.preset.insert(
+            {
+                ["<C-b>"] = cmap.scroll_docs(-4),
+                ["<C-f>"] = cmap.scroll_docs(4),
+                ["<C-o>"] = cmap.complete(),
+                ["<C-e>"] = cmap.abort(),
+                ["<CR>"] = cmap.confirm({select = true})
+            }
+        ),
+        snippet = {
+            expand = function(args)
+                require("luasnip").lsp_expand(args.body)
+            end
+        },
+        sources = cmp.config.sources(
+            {
+                {name = "nvim_lsp"},
+                {name = "luasnip"}
+            },
+            {
+                {name = "buffer"}
+            }
+        )
+    }
+)
+
