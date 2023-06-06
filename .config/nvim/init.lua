9d31a03cd4d4ad4e8679d6caec762523046d9f66
Xander Tedder <xandertedder7@gmail.com>
2023-06-05 22:56:44 -0400

    Amendment

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,7 +10,8 @@
 --[[ ⚙ ~
 From the instant I grasped the fragility of my configuration, it disgusted me. 
 I yearned for the vigor and conviction of Vim. I aimed to attain the purity of the Divine Editor.
-Your kind cling to your UI, as if it will not decay and fail you. One day the crude graphical interface that you call a temple will wither, and you will beg my kind to save you. But I am already saved, for Vim is everlasting...
+Your kind cling to your UI, as if it will not decay and fail you. One day the crude graphical interface that you call a temple will wither, and you will beg my kind to save you. 
+But I am already saved, for Vim is everlasting...
 ...even in death, I remain devoted to the Omnissiah. ~ ⚙ ]]--
 
 -- Initalize
@@ -29,4 +30,3 @@ require("language.snippet")
 
 -- Status line
 require("core.statusline")
-
