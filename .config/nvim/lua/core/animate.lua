a3c2e812a0c8135375c4557a7aad1d341eb26698
wnstdrr <xandertedder7@gmail.com>
2025-01-25 13:08:09 +0000

    Animations Added `mini.animate` library for smooth transitions throughout the UI

--- .config/nvim/lua/core/animate.lua
+++ .config/nvim/lua/core/animate.lua
@@ -1,37 +1,30 @@
---[[
---  █████╗ ███╗   ██╗██╗███╗   ███╗ █████╗ ████████╗███████╗
--- ██╔══██╗████╗  ██║██║████╗ ████║██╔══██╗╚══██╔══╝██╔════╝
--- ███████║██╔██╗ ██║██║██╔████╔██║███████║   ██║   █████╗  
--- ██╔══██║██║╚██╗██║██║██║╚██╔╝██║██╔══██║   ██║   ██╔══╝  
--- ██║  ██║██║ ╚████║██║██║ ╚═╝ ██║██║  ██║   ██║   ███████╗
--- ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝
-]]--
-
 local mini = require("mini.animate")
 mini.setup({
-    cursor = {
-        timing = mini.gen_timing.linear({
+    --[[cursor = {
+        enable = true,
+        timing = mini.gen_timing.quadratic({
             duration = 64, unit = "total"
-        })
-    },
-    scroll = {
-        timing = mini.gen_timing.linear({
+        }),
+    },--]]
+    --[[scroll = {
+        timing = mini.gen_timing.cubic({
             duration = 32, unit = "total"
-        })
-    },
+        }),
+    },--]]
     resize = {
-        timing = mini.gen_timing.linear({
+        timing = mini.gen_timing.exponential({
             duration = 6, unit = "total"
-        })
+        }),
     },
     open = {
         timing = mini.gen_timing.linear({
             duration = 64, unit = "total"
-        })
+        }),
     },
     close = {
         timing = mini.gen_timing.linear({
             duration = 64, unit = "total"
-        })
+        }),
     }
 })
+
