367bd6a8a7bd71f7442ee928760fac52eaec294b
wnstdrr <xandertedder7@gmail.com>
2025-01-25 12:49:45 +0000

    Require Board & Completion in Init Added require for `core.dboard` Added require for `language.completion`

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,32 +1,15 @@
---[[
-██╗███╗   ██╗██╗████████╗
-██║████╗  ██║██║╚══██╔══╝
-██║██╔██╗ ██║██║   ██║   
-██║██║╚██╗██║██║   ██║   
-██║██║ ╚████║██║   ██║   
-╚═╝╚═╝  ╚═══╝╚═╝   ╚═╝   
-]]--
-
 --[[ ⚙ ~
-From the instant I grasped the fragility of my configuration, it disgusted me. 
-I yearned for the vigor and conviction of Vim. I aimed to attain the purity of the Divine Editor.
-Your kind cling to your UI, as if it will not decay and fail you. One day the crude graphical interface that you call a temple will wither, and you will beg my kind to save you. 
-But I am already saved, for Vim is everlasting...
-...even in death, I remain devoted to the Omnissiah. ~ ⚙ ]]--
+    From the instant I grasped the fragility of my configuration, it disgusted me.
+        I yearned for the vigor and conviction of Vim. I aimed to attain the purity of the Divine Editor.
+        Your kind cling to your UI, as if it will not decay and fail you. One day the crude graphical interface that you call a temple will wither, and you will beg my kind to save you.
+        But I am already saved, for Vim is everlasting...
+        ...even in death, I remain devoted to the Omnissiah.
+~ ⚙ ]]--
 
--- Initalize
 require("plugins.plugin")
 require("core.bindings")
 require("core.schema")
-
--- Compression Utilities
-require("core.utilities.compression")
-
--- TODO: Git utilities, survived this far without it
-
--- Language servers
+require("core.dboard")
 require("language.lsp")
-require("language.snippet")
+require("language.completion")
 
--- Status line
-require("core.statusline")
