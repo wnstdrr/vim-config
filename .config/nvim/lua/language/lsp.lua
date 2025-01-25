de0ded74f78ee187168ec8f5b6ab2cabd91f1818
wnstdrr <xandertedder7@gmail.com>
2025-01-25 15:41:43 +0000

    Nice Reference Removed nice reference setup

--- .config/nvim/lua/language/lsp.lua
+++ .config/nvim/lua/language/lsp.lua
@@ -49,16 +49,3 @@ l.RegisterLanguages({
         filetype = "pyc",
     },
 })
-
--- Configure nice-reference popup display
-require('nice-reference').setup({
-    anchor = "NW",       -- Popup position anchor
-    relative = "cursor", -- Popup relative position
-    row = 1,             -- Popup x position
-    col = 0,             -- Popup y position
-    border = "rounded",  -- Popup borderstyle
-    winblend = 12,       -- Popup transaparency 0-100, where 100 is transparent
-    max_width = 128,     -- Max width of the popup
-    max_height = 64,     -- Max height of the popup
-    auto_choose = false, -- Go to reference if there is only one
-})
